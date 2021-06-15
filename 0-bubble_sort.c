0d309f67fcd49dc0584c6589f82014c676be1441
tennin12 <bassembenyhia12@gmail.com>
2021-06-15 23:12:31 +0100

    f0 added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sort an array of integers
+*
+* @array: pionter
+* @size: int
+*
+* Return: void
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
